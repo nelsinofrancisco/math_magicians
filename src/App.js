3e1f65bcfe80fff04e7852331ba65de60a45a71d
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-09-14 17:19:16 -0300

    Add class-based component in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      somethingUseless: true,
+    };
+  }
 
+  render() {
+    const { somethingUseless } = this.state;
+    if (somethingUseless) {
+      return (
+        <div className="App">
+          <Calculator />
+        </div>
+      );
+    }
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
+}
 export default App;
